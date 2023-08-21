ffd191633fdbd7e3ec33e5ee9a14405ce344859c
Juan Cássio <juancassiomarques@gmail.com>
2023-08-20 22:36:43 -0300

    add new project

---
+++ pequenos-projetos/cookieClicker/script.js
@@ -0,0 +1,49 @@
+let contadorMarcador = document.querySelector("#contador");
+
+let numero = 0;
+
+function verificarOpacidade() {
+  let imgElementClique = document.querySelector('img[alt="clique"]');
+  let imgElementVovo = document.querySelector('img[alt="vovo"]');
+  let imgElementFazenda = document.querySelector('img[alt="fazenda"]');
+  
+  if (numero <= 100) {
+    imgElementClique.style.opacity = 0.5;
+    imgElementVovo.style.opacity = 0.5;
+    imgElementFazenda.style.opacity = 0.5;
+  } else if (numero >= 100 && numero < 120) {
+    imgElementClique.style.opacity = 1;
+    imgElementVovo.style.opacity = 0.5;
+    imgElementFazenda.style.opacity = 0.5;
+  } else if (numero >= 120 && numero < 150) {
+    imgElementClique.style.opacity = 1;
+    imgElementVovo.style.opacity = 1;
+    imgElementFazenda.style.opacity = 0.5;
+  } else if (numero >= 150) {
+    imgElementClique.style.opacity = 1;
+    imgElementVovo.style.opacity = 1;
+    imgElementFazenda.style.opacity = 1;
+  }
+}
+
+function atualizarNumero() {
+  contadorMarcador.textContent = numero;
+  verificarOpacidade();
+}
+
+let cookieButton = document.querySelector("#cookieButton");
+cookieButton.addEventListener('click', function(){
+  numero = parseInt(contadorMarcador.textContent) + 1;
+  atualizarNumero();
+});
+
+document.addEventListener('keyup', function(){
+  numero = parseInt(contadorMarcador.textContent) + 1;    
+  atualizarNumero();
+});
+
+let buttonReiniciar = document.querySelector("#buttonReiniciar");
+buttonReiniciar.addEventListener('click', function() {
+  numero = 0;
+  atualizarNumero();
+});
